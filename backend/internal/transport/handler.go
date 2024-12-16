d84180ef52d94179b28acabdd568542e3c384414
KatyNester <nesterenkokm@bk.ru>
2024-12-17 00:16:36 +0300

    Написан фронт для главной страницы, авторизации, регистрации.

--- backend/internal/transport/handler.go
+++ backend/internal/transport/handler.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/katenester/Web_chat2/backend/internal/service"
+	"path/filepath"
 )
 
 type Handler struct {
@@ -17,22 +18,28 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	// Раздача статических файлов из директории "frontend"
-	router.Static("/static", "./frontend2")
+	//router.Static("/static", "./frontend2")
+	//
+	//// Рендеринг HTML страниц из директории "frontend"
+	//router.GET("/", func(c *gin.Context) {
+	//	c.File("./frontend2/index.html") // Главная страница
+	//})
+	//router.GET("/auth/register", func(c *gin.Context) {
+	//	c.File("./frontend2/register.html") // Страница регистрации
+	//})
+	//router.GET("/auth/login", func(c *gin.Context) {
+	//	c.File("./frontend2/c.html") // Страница авторизации
+	//})
+	//router.GET("/chats", func(c *gin.Context) {
+	//	c.File("./frontend2/chats.html") // Страница чатов
+	//})
+	publicPath := filepath.Join(".", "frontend2")
 
-	// Рендеринг HTML страниц из директории "frontend"
-	router.GET("/", func(c *gin.Context) {
-		c.File("./frontend2/index.html") // Главная страница
-	})
-	router.GET("/register", func(c *gin.Context) {
-		c.File("./frontend2/register.html") // Страница регистрации
-	})
-	router.GET("/login", func(c *gin.Context) {
-		c.File("./frontend2/login.html") // Страница авторизации
-	})
-	router.GET("/chats", func(c *gin.Context) {
-		c.File("./frontend2/chats.html") // Страница чатов
-	})
-
+	// static
+	router.StaticFile("/", filepath.Join(publicPath, "index.html"))
+	router.StaticFile("/auth/register", filepath.Join(publicPath, "register.html"))
+	router.StaticFile("/auth/login", filepath.Join(publicPath, "login.html"))
+	router.StaticFile("/chats", filepath.Join(publicPath, "chats.html"))
 	// Группа маршрутов для аутентификации
 	auth := router.Group("/auth")
 	{
