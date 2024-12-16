3fc30b6d04cd8554efb6ab83e33d596fd02c8060
KatyNester <nesterenkokm@bk.ru>
2024-12-16 21:39:02 +0300

    Настройка окружения для Тани

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
 	"github.com/katenester/Web_chat2/backend/internal/repository"
 	"github.com/katenester/Web_chat2/backend/internal/repository/sqllite/config"
 	"github.com/katenester/Web_chat2/backend/internal/service"
 	"github.com/katenester/Web_chat2/backend/internal/transport"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
@@ -25,22 +21,25 @@ func main() {
 	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := transport.NewHandler(services)
-	go func() {
-		if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-			logrus.Fatalf("error occured while running http server %s", err.Error())
-		}
-	}()
-
-	logrus.Print("todo server started")
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
-	<-quit
-
-	logrus.Println("shutting down server...")
-	if err := srv.Shutdown(context.Background()); err != nil {
-		logrus.Fatalf("error occured while shutting down server %s", err.Error())
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occured while running http server %s", err.Error())
 	}
-	if err := db.Close(); err != nil {
-		logrus.Fatalf("error occured while closing db %s", err.Error())
-	}
+	//go func() {
+	//	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	//		logrus.Fatalf("error occured while running http server %s", err.Error())
+	//	}
+	//}()
+
+	//logrus.Print("todo server started")
+	//quit := make(chan os.Signal, 1)
+	//signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	//<-quit
+	//
+	//logrus.Println("shutting down server...")
+	//if err := srv.Shutdown(context.Background()); err != nil {
+	//	logrus.Fatalf("error occured while shutting down server %s", err.Error())
+	//}
+	//if err := db.Close(); err != nil {
+	//	logrus.Fatalf("error occured while closing db %s", err.Error())
+	//}
 }
